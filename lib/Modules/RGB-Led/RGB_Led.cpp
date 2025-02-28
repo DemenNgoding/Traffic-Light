ab007906ed9f969a962df9093d83068e87121192
DemenNgoding <alfianeff123@gmail.com>
2025-02-28 17:12:34 +0700

    vscode config

---
+++ lib/Modules/RGB-Led/RGB_Led.cpp
@@ -0,0 +1,21 @@
+#include "RGB_Led.h"
+
+rgbLed::rgbLed(int redPin, int greenPin, int bluePin)
+    : _redPin(redPin), _greenPin(greenPin), _bluePin(bluePin) {}
+
+void rgbLed::begin() {
+    pinMode(_redPin, OUTPUT);
+    pinMode(_greenPin, OUTPUT);
+    pinMode(_bluePin, OUTPUT);
+    off();
+}
+
+void rgbLed::setColor(int red, int green, int blue) {
+    analogWrite(_redPin, red);
+    analogWrite(_greenPin, green);
+    analogWrite(_bluePin, blue);
+}
+
+void rgbLed::off() {
+    setColor(0, 0, 0);
+}
